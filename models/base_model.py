8b3c0b303c156686f4312f46ef6d5805bc68804e
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-07-05 20:47:02 -0300

    Trying to fix some checks

--- models/base_model.py
+++ models/base_model.py
@@ -7,22 +7,28 @@ from datetime import datetime
 
 
 class BaseModel:
-    """ """
+    """ BaseModel class that defines all """
 
     def __init__(self):
+        """ initializes the class instance """
         self.id = str(uuid.uuid4())
         self.updated_at = datetime.now()
         self.created_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
+        """ returns a string representation of the class
+            instance attributes """
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """ updates the public instance attribute
+            updated_at with the current datetime """
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """ """
+        """ returns a dictionary of the class
+            instance attributes  """
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
