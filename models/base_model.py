7361160bcfbf2e8e7608dd763f48a0b074fd79d8
Ismael Molina <6214@holbertonstudents.com>
2023-07-05 15:02:02 -0300

    Testing the first base model excercise

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+""" BaseModel class that defines all
+    common attributes/methods for other classes """
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """ """
+
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.updated_at = datetime.now()
+        self.created_at = datetime.now()
+
+    def __str__(self):
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        return {
+            'my_number': self.my_number,
+            'name' : self.name,
+            '__class__': self.__class__.__name__,
+            'created_at': self.created_at.isoformat(),
+            'id' : self.id,
+            'updated_at': self.updated_at.isoformat()
+            }
