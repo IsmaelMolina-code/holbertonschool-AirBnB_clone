fdf704e478e9822a27f29b99cdfd3ae2f35bc61b
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-07-06 20:53:05 -0300

    Fixing Task 4 of Airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,12 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         if kwargs:
-            if key == '__class__':
-                kwargs.pop(key)
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
                     time_value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, time_value)
-                setattr(self, key, value)
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
