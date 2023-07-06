b64f4e4b9533f0b128a5cfb11cb31d651a456b59
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-07-06 20:37:40 -0300

    Task 4 of Airbnb clone

--- models/base_model.py
+++ models/base_model.py
@@ -9,8 +9,14 @@ from datetime import datetime
 class BaseModel:
     """ BaseModel class that defines all """
 
-    def __init__(self):
-        """ initializes the class instance """
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == 'created_at' or key == 'updated_at':
+                    time_value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, time_value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
