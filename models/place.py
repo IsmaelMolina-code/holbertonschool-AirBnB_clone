cee7fba914a37da01683a33154584e36b4caf60d
lataba <ltballeste@gmail.com>
2023-07-08 15:34:55 -0700

    adding class BaseModel to inherit

--- models/place.py
+++ models/place.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Defines the class Place that inherit from BaseModel"""
+
+from models.base_model import BaseModel
 
 
-
-class Place:
+class Place(BaseModel):
+    """Create a new empty Place class"""
 
     city_id = ""
     user_id = ""
--- models/review.py
+++ models/review.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""Defines the class Review that inherit from BaseModel"""
+
+from models.base_model import BaseModel
 
 
-
-class Review:
+class Review(BaseModel):
+    """Create a new review empty class"""
 
     place_id = ""
     user_id = ""
