c14c14f2066f9a7b676a5fdefe6e5310b266b31e
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-07-08 01:57:15 -0300

    Implementation of the console entry point of the command interpreter

--- console.py
+++ console.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" Console Module """
+
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """ Console class """
+    def __init__(self):
+        cmd.Cmd.__init__(self)
+        self.prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """ Quit command to exit the program """
+        return True
+
+    def do_EOF(self, arg):
+        """ EOF command to exit the program """
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
