d175ed2d5fbdf419c2069a55b394c34ec3c46486
Ismael Molina Molina <ismaelmolina544@gmail.com>
2023-07-07 22:46:10 -0300

    Trying fixes

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,8 @@ class BaseModel:
                     time_value = datetime.strptime(value,
                                                    "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, key, time_value)
+                if key != "__class__":
+                    setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -41,6 +43,8 @@ class BaseModel:
             instance attributes  """
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
+        if type(self.created_at) != str:
+            new_dict["created_at"] = self.created_at.isoformat()
+        if type(self.updated_at) != str:
+            new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
